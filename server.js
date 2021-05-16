ebe2be34c4bebfdba99eb4a39b214c35a8956ade
Gokul s <samigokul474@gmail.com>
2021-05-16 22:16:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: "unnamedbot2oo5@gmail.com", 
-        pass: process.env.GMAIL_PASSWORD 
+        pass: "Liverpool@2019"
     }
   });
 
@@ -115,7 +115,7 @@ app.post("/email",async (req,res)=>{
                     from: 'unnamedbot2oo5@gmail.com', // TODO: email sender
                     to: req.body.email, // TODO: email receiver
                     subject: 'Password reset',
-                    text: `Reset your password using the below link : http://localhost:3000/resetpassword/${user[0]._id}`
+                    text: `Reset your password using the below link : https://objective-goodall-0f6a6a.netlify.app/resetpassword/${user[0]._id}`
                 };
                 
                 // Step 3
@@ -328,42 +328,3 @@ function verification(req,res,next)
       })  
   }
 }
-
-
-cron.schedule('* * * * *',  async() => {
-    let connection = await mongodb.connect(URL);
-    let db = connection.db(DB);
-    let task = await db.collection("Task").find({"Completed":false}).toArray();
-    if(task.length!=0)
-    {
-      let date  = new Date();
-      let currentmonth = date.getMonth();
-      let currentdate = date.getDate();
-      let currenthour = date.getHours();
-      let currentmin = date.getMinutes();
-
-      
-      task.map(async(i)=>{
-          let temp = i.TaskTime.split(":");
-          if(i.Taskmonth===currentmonth&&i.Taskdate===currentdate&&+temp[0]===currenthour&&+temp[1]===currentmin)
-          {
-             let user = await db.collection("User").find({_id:mongodb.ObjectID(i.Userid)}).toArray();
-             console.log(user);
-             const message = {
-                from: 'unnamedbot2oo5@gmail.com', // Sender address
-                to: user[0].email,         // List of recipients
-                subject: `Remainder for task-${i.Taskname}`, // Subject line
-                text: `Hello ${user[0].Name}, \n\n It's time to start ${i.Taskname}. \n\n Use the  link below to registry the task completion or to cancel the task: \n\n http://localhost:3000/remainder/${i._id}` // Plain text body
-            };
-            transport.sendMail(message, function(err, info) {
-                if (err) {
-                  console.log(err)
-                } else {
-                  console.log(info);
-                }
-            });
-          }
-      })
-    }
-    connection.close();
- });
